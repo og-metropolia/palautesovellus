d48c82a8eff7caa75f0f2b14c1a5f8de358e1e00
4MBL <74970679+4MBL@users.noreply.github.com>
2023-09-28 12:44:27 +0300

    fix login state in signup, enum for storage keys

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import TeachersQuestion from '../components/TeachersQuestion';
 import routes from '../constants/routes.mjs';
+import { LOCAL_STORAGE_KEYS } from '../constants/local-storage.mjs';
 
 function App() {
-  const userId = window.localStorage.getItem('teacherId');
+  const userId = window.localStorage.getItem(LOCAL_STORAGE_KEYS.userId);
 
   if (userId) {
     return (
       <div>
-      <TeachersQuestion content="Opettajan kysymys" color="black" backgroundColor="lightgray" userId={userId} />
-    </div>
-  );
+        <TeachersQuestion
+          content="Opettajan kysymys"
+          color="black"
+          backgroundColor="lightgray"
+          userId={userId}
+        />
+      </div>
+    );
   } else {
-      window.location.href = routes.login
+    window.location.href = routes.login;
   }
 }
 
