f283aec1f3a9ca7eb46e733a9f2d3647eaf324c4
4MBL <74970679+4MBL@users.noreply.github.com>
2023-10-06 17:24:17 +0300

    fix api path

--- src/constants/api.mjs
+++ src/constants/api.mjs
@@ -9,5 +9,9 @@ export const ENDPOINTS = Object.freeze({
 export const API_PATH = 'api/v0';
 
 export const BASE_URL = import.meta.env
-  ? import.meta.env.VITE_API_HOST + ':' + import.meta.env.VITE_API_PORT
-  : 'http://localhost:3000';
+  ? import.meta.env.VITE_API_HOST +
+    ':' +
+    import.meta.env.VITE_API_PORT +
+    '/' +
+    API_PATH
+  : `http://localhost:3000/${API_PATH}`;
